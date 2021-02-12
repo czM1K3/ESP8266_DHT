cbe6d5aa072e7f7f6b0063a5ae07bb794c9ba5ae
czM1K3 <misamadera1@gmail.com>
2021-02-12 15:28:47 +0100

    Added ArduinoOTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
@@ -37,10 +38,50 @@ void setup() {
   server.on("/", sendData);
   server.begin();
   dht.begin();
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {
+      type = "filesystem";
+    }
+#ifdef SERIAL_BAUD
+    Serial.println("Start updating " + type);
+#endif
+  });
+  ArduinoOTA.onEnd([]() {
+#ifdef SERIAL_BAUD
+    Serial.println("\nEnd");
+#endif
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+#ifdef SERIAL_BAUD
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+#endif
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+#ifdef SERIAL_BAUD
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+#endif
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
   server.handleClient();
+  ArduinoOTA.handle();
 }
 
 void sendData() {
